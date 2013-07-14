0d3f06336cfc7c5065cf1441525c3bff9e34b903
qxo <qxodream@gmail.com>
2013-07-14 11:44:48 +0800

    fixed for  OptionBean

--- wabacus-extra/src/com/wabacus/extra/options/OptionBean.java
+++ wabacus-extra/src/com/wabacus/extra/options/OptionBean.java
@@ -18,136 +18,29 @@
  */
 package com.wabacus.extra.options;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.wabacus.config.component.application.report.ConditionBean;
 import com.wabacus.config.xml.XmlElementBean;
-import com.wabacus.util.RegexTools;
 
-public class OptionBean extends XmlElementBean  implements Cloneable
-{
-
-    private int sourceType;
-
-    private String sql;
-
-    private String label="";
-
-    private String value="";
-
-    private String[] type;
-
-    private List<ConditionBean> lstConditions=new ArrayList<ConditionBean>();//当为SQL语句查询选项数据时，且需要从request/session中获取条件数据，这里存放所有的条件bean
+public class OptionBean extends XmlElementBean implements Cloneable {
 
     public OptionBean(String name) {
         super(name);
         this.setLabel(name);
     }
 
-    public void setLabel(String label)
-    {
-        this.label=label;
+    public void setLabel(String label) {
+        this.setAttributeValue("label", label);
     }
 
-    public void setValue(String value)
-    {
-        this.value=value;
+    public void setValue(String value) {
+        this.setAttributeValue("value", value);
     }
 
-    public String getLabel()
-    {
-        return this.label;
+    public String getLabel() {
+        return this.attributeValue("label");
     }
 
-    public String getValue()
-    {
-        return this.value;
+    public String getValue() {
+        return this.attributeValue("value");
     }
 
-    public String[] getType()
-    {
-        return type;
-    }
-
-    public void setType(String[] type)
-    {
-        this.type=type;
-    }
-
-    public int getSourceType()
-    {
-        return sourceType;
-    }
-
-    public void setSourceType(int sourceType)
-    {
-        this.sourceType=sourceType;
-    }
-
-    public String getSql()
-    {
-        return sql;
-    }
-
-    public void setSql(String sql)
-    {
-        this.sql=sql;
-    }
-
-    public List<ConditionBean> getLstConditions()
-    {
-        return lstConditions;
-    }
-
-    public void setLstConditions(List<ConditionBean> lstConditions)
-    {
-        this.lstConditions=lstConditions;
-    }
-
-    public boolean isMatch(String parentValue,boolean isRegex)
-    {
-        if(this.type==null||this.type.length==0)
-        {//没有配置type，则与父下拉框所选中的选项无关，一直显示出来
-            return true;
-            
-            
-        }
-        if(this.type.length==1&&this.type[0].equals("%true-true%"))
-        {
-            return false;
-        }else if(this.type.length==1&&this.type[0].equals("%false-false%"))
-        {
-            return false;
-        }
-        if(parentValue==null) return false;
-        for(int i=0;i<type.length;i++)
-        {
-            if(!isRegex&&parentValue.equals(type[i])) return true;
-            if(isRegex&&RegexTools.isMatch(parentValue,type[i])) return true;
-        }
-        return false;
-    }
-
-    protected Object clone()
-    {
-        OptionBean obNew=null;
-        try
-        {
-            obNew=(OptionBean)super.clone();
-            if(this.lstConditions!=null)
-            {
-                List<ConditionBean> lstConNew=new ArrayList<ConditionBean>();
-                for(ConditionBean cbTmp:this.lstConditions)
-                {
-                    lstConNew.add((ConditionBean)cbTmp.clone(null));
-                }
-                obNew.setLstConditions(lstConNew);
-            }
-        }catch(CloneNotSupportedException e)
-        {
-            e.printStackTrace();
-        }
-        return obNew;
-    }
 }
